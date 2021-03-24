772af4f23affe68322af841411e59a815e2cb354
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-03-24 19:55:52 +0530

    re-added husky and configured it for prettier

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
-import Header from './Header';
-import ErrorBoundary from './ErrorBoundary';
+import React from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
+import Header from "./Header";
+import ErrorBoundary from "./ErrorBoundary";
 
-import './MainPage.css';
+import "./MainPage.css";
 
 class MainPage extends React.Component {
+  render() {
+    const {
+      robots,error,
+      isPending,
+searchField,
+      onSearchChange,
+    } = this.props;
 
-    render() {
+    const filteredRobots = robots.filter((robot) =>
+      robot.name.toLowerCase().includes(searchField.toLowerCase())
+    );
 
-            const { robots, error, isPending, 
-                searchField, onSearchChange 
-                 } = this.props;
-                 
-
-        const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
-
-        if (isPending) {
-            return <h1>LOADING</h1>
-        } else if (error) {
-            return <h1>Failed to fetch the Robots</h1>
-        }
-        else {
-            return (
-                <div className="tc">
-                    <Header />
-                    <SearchBox searchChange={onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            )
-        }
+    if (isPending) {
+      return <h1>LOADING</h1>;
+    } else if (error) {
+      return <h1>Failed to fetch the Robots</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <Header />
+          <SearchBox searchChange={onSearchChange} />
+          <Scroll>
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
+          </Scroll>
+        </div>
+      );
     }
+  }
 
-    componentDidMount() {
-        this.props.getRobots();
-    }
+  componentDidMount() {
+    this.props.getRobots();
+  }
 }
 
 export default MainPage;
